d65012a5ffec7a6b45870ecd3d356c0d4f426210
François-Célestin <82862772+Fc85@users.noreply.github.com>
2021-10-04 00:36:26 +0200

    Update connection_db.php

--- scripts/connection_db.php
+++ scripts/connection_db.php
@@ -2,10 +2,10 @@
 try
 {
 // Préparation des paramètres du constructeur
-$user="passi1614852";
-$password="passi1614852"; //mot de passe de MySQL
-$base="passi1614852";
-$host="91.216.107.183";
+$user="";
+$password=""; //mot de passe de MySQL
+$base="";
+$host="";
 $SGBD="mysql:host=$host;dbname=$base";
 $options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION; //Captage des exceptions
 $bdd = new PDO($SGBD, $user, $password, $options); //construction de $bdd
